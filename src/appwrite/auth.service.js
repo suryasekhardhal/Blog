53687c7ea27452d6c563b8435860ce73e96fcbb0
suryasekhar2003 <suryasekhardhal2003@gmail.com>
2026-01-22 09:17:45 +0530

    fix: correct parameter format in login method call during account creation

--- src/appwrite/auth.service.js
+++ src/appwrite/auth.service.js
@@ -17,7 +17,7 @@ export class AuthService {
             const userAccount = await this.account.create(ID.unique(),email,password,name)
             if (userAccount) {
                 //call another method
-                return this.login(email,password)
+                return this.login({email,password})
             } else {
                 return userAccount
             }
